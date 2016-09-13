ae1c11fa6942b4ebe8381db179031801fc9d8eec
Nelson Domínguez <nelson.dominguez@odigeo.com>
2016-09-13 17:48:34 +0200

    Add ActionBar and ActionBarManager

---
+++ ActionBarManager/ActionBarManager.h
@@ -0,0 +1,19 @@
+//
+//  ActionBarManager.h
+//  ActionBarManager
+//
+//  Created by Nelson Dominguez Leon on 13/09/16.
+//  Copyright © 2016 ODIGEO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionBarManager.
+FOUNDATION_EXPORT double ActionBarManagerVersionNumber;
+
+//! Project version string for ActionBarManager.
+FOUNDATION_EXPORT const unsigned char ActionBarManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionBarManager/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "8.0"
+use_frameworks!
+
+target 'ActionBarManager' do
+	pod 'TableViewKit', :git => 'https://github.com/odigeoteam/TableViewKit.git', :branch => 'swift3'
+end
+
+target 'ActionBarManagerTests' do
+
+end
