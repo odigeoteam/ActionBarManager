49ecced759be15b381479f34e91ba86b7684eb6d
Nelson Domínguez <nelson.dominguez@odigeo.com>
2016-09-15 12:53:31 +0200

    Add .travis.yml

---
+++ build.sh
@@ -0,0 +1,7 @@
+set -o pipefail &&
+time xcodebuild clean test \
+    -workspace ActionBarManager.xcworkspace \
+    -scheme ActionBarManager \
+    -sdk iphonesimulator10.0 \
+    -destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.0' \
+| xcpretty
